b5840f4422dc6c140ab74cff7df23edb833007a8
kasineeboontawe <boontawekasinee@gmail.com>
2018-06-26 12:20:29 +0700

    Initial commit

---
+++ src/lab1/myCode.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class myCode  {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("kasinee boontawe");
+        System.out.println("361211760023");
+        System.out.println("MIT211");
+        System.out.println("rmutsv");
+
+    }
+}
