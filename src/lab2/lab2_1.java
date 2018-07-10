79fde56a772772506d29ccf8564ad5792bf14846
email <boontawekasinee@gmail.com>
2018-07-10 11:42:21 +0700

    number

---
+++ src/lab2/lab2_1.java
@@ -0,0 +1,62 @@
+package lab2;
+
+public class lab2_1 {
+
+    public static void main(String[] args) {
+
+    int w;
+    w = 2;
+    System.out.println(w);
+    int e;
+    e = 4;
+    System.out.println(e);
+    int r;
+    r = 6;
+    System.out.println(r);
+    int t;
+    t = 8;
+    System.out.println(t);
+    int y;
+    y = 10;
+    System.out.println(y);
+    int u;
+    u = 12;
+    System.out.println(u);
+    int i;
+    i = 14;
+    System.out.println(i);
+    int s;
+    s = 16;
+    System.out.println(s);
+    int d;
+    d = 18;
+    System.out.println(d);
+    int f;
+    f = 20;
+    System.out.println(f);
+    int z = w+e+r+t+y+u+i+s+d+f;
+    System.out.println(z);
+    int n;
+    n = 110;
+    System.out.println(n);
+    int v;
+    v = 10;
+    System.out.println(v);
+    System.out.println(n/v);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+}//class
