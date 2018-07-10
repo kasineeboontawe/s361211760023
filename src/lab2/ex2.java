a79048ad2ffeb465ea5ac179b8753c544c4e3bb0
email <boontawekasinee@gmail.com>
2018-07-10 10:10:35 +0700

    input data with System.in.read()

---
+++ src/lab2/ex2.java
@@ -0,0 +1,42 @@
+package lab2;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        double number;
+        number = 15;
+        System.out.println(number);
+
+        char c = 'm';
+        System.out.println(c);
+
+        String msg;
+        msg = "kasinee boontawe";
+        System.out.println(msg);
+        String  msg2 = " MIT";
+
+        System.out.println(msg+msg2);
+
+        //String is a Class in java
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toUpperCase());
+
+        //boolean
+        boolean m = true;
+        System.out.println(m);
+        m = false;
+        System.out.println(m);
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+}//class
---
+++ src/lab2/exread.java
@@ -0,0 +1,29 @@
+package lab2;
+
+import java.io.IOException;
+
+public class exread {
+    public static void main(String[] args)throws IOException {
+ //       char c ;
+  //      System.out.println(2);
+  //      c = (char) System.in.read();
+   //     System.out.println(3+c);
+
+        System.out.println("How own are you? : ");
+        int age = 0;
+        age =(int)System.in.read();
+        System.out.println("your are "+age+" ywars old");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
