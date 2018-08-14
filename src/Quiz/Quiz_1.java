2832a813da5dff0337ad5667f8b7d4b676afb5b2
kasineeboontawe <boontawekasinee@gmail.com>
2018-08-14 11:57:42 +0700

    Quiz

---
+++ src/Quiz/Quiz_1.java
@@ -0,0 +1,32 @@
+package Quiz;
+
+public class Quiz_1 {
+    public static void main(String[] args) {
+
+        for (int i = 500; i < 1000; i++) {
+            //statement for
+            if (i % 3 == 0 || i % 6 == 0)
+
+                System.out.println(i + " ");
+
+            if (i%500==0 || i%1000==0)
+
+                System.out.println("ผลลัพธ์ที่ได้กี่จำนวน");
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+        }//main
+}//calss
---
+++ src/Quiz/Quiz_3.java
@@ -0,0 +1,13 @@
+package Quiz;
+
+public class Quiz_3 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }//main
+}//class
