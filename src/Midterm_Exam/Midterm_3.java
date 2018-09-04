f0daf10ca89822aa8994d7158af88d8d8b518931
kasineeboontawe <boontawekasinee@gmail.com>
2018-09-04 11:58:04 +0700

    Midterm_Exam

---
+++ src/Midterm_Exam/Midterm_3.java
@@ -0,0 +1,10 @@
+package Midterm_Exam;
+
+public class Midterm_3 {
+    public static void main(String[] args) {
+
+
+
+
+    }//main
+}//class
---
+++ src/Midterm_Exam/Midterm_4.java
@@ -0,0 +1,38 @@
+package Midterm_Exam;
+
+import java.util.Scanner;
+
+public class Midterm_4 {
+    public static void main(String[] args) {
+        int Select;
+        double Price=0,total=0 ;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("###### Please select Room type ######");
+
+        System.out.print( "Type 1 10%");
+        System.out.println("เลือกการเสียภาษีตามการแยกประเภทสินค้า ?");
+        System.out.println("Type 1.10 %");
+        System.out.println("Type 2.15 %");
+        System.out.println("Type 3.17 %");
+        System.out.println("Type 4.20 %");
+
+        System.out.print("Select: ");
+        Select = sc.nextInt();
+        System.out.print("price: ");
+        Price =sc.nextInt();
+
+
+        switch (Select){
+            case 1: total = Price + (Price * 0.1);break;
+            case 2: total = Price + (Price * 0.15);break;
+            case 3: total = Price + (Price * 0.17);break;
+            case 4: total = Price + (Price * 0.20);break;
+            default: System.out.print("Please select product type (1-4)");
+        }
+        System.out.println("Total Price"+total);
+
+
+
+    }//main
+
+}//class
