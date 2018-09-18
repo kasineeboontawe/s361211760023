a2bb2a50bb9e0312384f172dd44d3be21604fe7f
kasineeboontawe <boontawekasinee@gmail.com>
2018-09-18 09:25:24 +0700

    Array and arrayList

--- src/lab5/ex3.java
+++ src/lab5/ex3.java
@@ -1,4 +1,11 @@
 package lab5;
 
 public class ex3 {
-}
+    public static void main(String[] args) {
+        System.out.println("Input data to Array 2D:");
+        //create array
+        int[][] num = new int[2][3]; //2 rows and 3 columns = 6 elements
+
+        inputData (num);
+    }//main
+}//class
